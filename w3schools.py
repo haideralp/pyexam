0b08cf04d1285539ad352eac59135ad3ac18cba2
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-05 12:59:55 +0100

    update

--- w3schools.py
+++ w3schools.py
@@ -17,4 +17,19 @@ more that just one line
 # Python Variables
 # create car variable and assign Volvo as value
 carname = "Volvo"
+x = 50  # assigning value to x
+
+# Display the sum of 5 + 10, using two variables: x and y.
+
+x = 5
+y = 10
+print(x + y)
+
+# Create a variable called z, assign x + y to it, and display the result.
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
 
