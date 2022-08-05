b35fc4e9c5db768c60b402ad225bc4fecb49cc80
Syed Haider Abedi <haider.abedi@gmx.com>
2022-08-05 12:36:27 +0100

    w3exercises

---
+++ w3schools.py
@@ -0,0 +1,20 @@
+# Exercises on W3schools - Code
+
+# Python Syntax
+
+print("Hello World!")  # using print function to displau message
+if 5 > 2:
+    print("Five is greater than two!")  # corrected indentation error using tab.
+
+# Python Commenting
+# This is a comment   """ - used to comment multiple lines.
+""" 
+This is a comment    
+written in 
+more that just one line
+"""
+
+# Python Variables
+# create car variable and assign Volvo as value
+carname = "Volvo"
+
